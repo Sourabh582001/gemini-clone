439dda1597ba13b0205b16ec5a6051a7781d26db
Sourabh Kumar <sourabhkumar582001@gmail.com>
2025-08-06 14:00:46 +0530

    fix: removed explicit any from fetchCountries

--- src/utils/fetchCountries.ts
+++ src/utils/fetchCountries.ts
@@ -1,11 +1,18 @@
 
+interface Country {
+    name: { common: string };
+    idd?: {
+        root?: string;
+        suffixes?: string[];
+    };
+}
 
 export async function fetchCountries(): Promise<{ name: string; code: string }[]> {
     const res = await fetch("https://restcountries.com/v3.1/all?fields=name,idd");
-    const data: any[] = await res.json();
+    const data: Country[] = await res.json();
 
     return data.map((country) => ({
         name: country.name.common,
-        code: country.idd?.root + (country.idd?.suffixes?.[0] || ''),
+        code: (country.idd?.root || '') + (country.idd?.suffixes?.[0] || ''),
     }));
 }
